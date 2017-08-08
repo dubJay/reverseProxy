f3899b2208016c200087da4a9a1c39d5b68ee800
dubJay <cawdreychris@yahoo.com>
2017-08-08 00:44:27 -0700

    Added support for static server balancing as well. Also it was too easy to parse so I added a bunch of confusing pointers. Can't wait to hate myself in a few months

--- src/github.com/dubJay/reverseProxy/reverseProxy.go
+++ src/github.com/dubJay/reverseProxy/reverseProxy.go
@@ -10,46 +10,51 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
 var (
-	cert      = flag.String("cert", "", "Concatenation of server's certificate, any intermediates, and the CA's certificate")
-	hosts = flag.String("hosts", "", "CSV of all hosts to load balance across")
-	key       = flag.String("key", "", "Private key for TLS")
-	port  = flag.String("port", ":8080", "Port for server to listen on")
-	rootDir   = flag.String("rootDir", "", "Path to webdir structure")
-	ssl       = flag.Bool("ssl", false, "Whether to use TLS")
-	queue chan *url.URL
+	cert        = flag.String("cert", "", "Concatenation of server's certificate, any intermediates, and the CA's certificate")
+	hosts       = flag.String("hosts", "", "CSV of all hosts to load balance across")
+	staticHosts = flag.String("static_hosts", "", "CSV of all static hosts to load balance across")
+	key         = flag.String("key", "", "Private key for TLS")
+	port        = flag.String("port", ":8080", "Port for server to listen on")
+	rootDir     = flag.String("rootDir", "", "Path to webdir structure")
+	ssl         = flag.Bool("ssl", false, "Whether to use TLS")
 )
 
+type queues struct{
+	queue chan *url.URL
+	staticQueue chan *url.URL
+}
+
 // Simple round robin queue. ATM all backend comps have equal compute power.
-func seedQueue(allHosts []string) error {
-	queue = make(chan *url.URL, len(allHosts))
-	// Populate queue.
-	for _, host := range allHosts {
+func populateQueue(hosts []string, queue *chan *url.URL) error {
+	tmp := make(chan *url.URL, len(hosts))
+	// Populate queues.
+	for _, host := range hosts {
 		// TODO: Validate host can be reached.
 		url, err := url.Parse(host)
 		if err != nil {
 			log.Printf("Error parsing url %s: %v", host, err)
 		}
-		queue <- url
+		tmp <- url
 	}
-	if len(allHosts) == 0 {
+	if len(hosts) == 0 {
 		return errors.New("No hosts provided for reverse proxy")
-	}	
+	}
+
+	*queue = tmp
 	return nil
 }
 
-func getNext() *url.URL {
-	if len(queue) == 0 {
+func getNext(queue *chan *url.URL) *url.URL {
+	if len(*queue) == 0 {
 		// Rare case that may occur if under heavy load.
 		log.Printf("Queue empty at %v", time.Now().Unix())
 		return &url.URL{}
 	}
-	var host = <-queue
-	queue <- host
+	var host = <-*queue
+	*queue <- host
 	return host
 }
 
@@ -66,11 +71,11 @@ func singleJoiningSlash(a, b string) string {
 	return a + b
 }
 
-func newMultiHostReverseProxy() *httputil.ReverseProxy {
+func newMultiHostReverseProxy(queue chan *url.URL) *httputil.ReverseProxy {
 	// Remnant of SingleHostReverseProxy.
 	targetQuery := ""
 	director := func(req *http.Request) {
-		var target = getNext()
+		var target = getNext(&queue)
 		req.URL.Scheme = target.Scheme
 		req.URL.Host = target.Host
 		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
@@ -98,21 +103,30 @@ func redirect(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	flag.Parse()
-	
-	err := seedQueue(strings.Split(*hosts, ","))
+
+	queues := &queues{}
+	err := populateQueue(strings.Split(*hosts, ","), &queues.queue)
 	if err != nil {
 		log.Fatal(err)
 	}
-	rp := newMultiHostReverseProxy()
+	err = populateQueue(strings.Split(*staticHosts, ","), &queues.staticQueue)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	rp := newMultiHostReverseProxy(queues.queue)
+	srp := newMultiHostReverseProxy(queues.staticQueue)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", rp.ServeHTTP).Methods("GET")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/static/", srp.ServeHTTP)
+	mux.HandleFunc("/images/", srp.ServeHTTP)
+	mux.HandleFunc("/", rp.ServeHTTP)
 
 	if !*ssl {
-		log.Fatal(http.ListenAndServe(*port, router))
+		log.Fatal(http.ListenAndServe(*port, mux))
 	} else {
 		go http.ListenAndServe(":80", http.HandlerFunc(redirect))
 		log.Fatal(http.ListenAndServeTLS(
-			*port, filepath.Join(*rootDir, *cert), filepath.Join(*rootDir, *key), router))
+			*port, filepath.Join(*rootDir, *cert), filepath.Join(*rootDir, *key), mux))
 	}
 }
